48f6b394c7a42fcd5a7694d14c7a284ef086fb04
Vedavyasa Rao <33799212+VedavyasaRao@users.noreply.github.com>
2026-03-10 17:38:44 +0530

    updates

--- User Interface Automation/Examples/Calucalator/compile.cmd
+++ User Interface Automation/Examples/Calucalator/compile.cmd
@@ -1,4 +1,4 @@
 pushd .
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe  /target:exe /out:runcalc.exe /platform:x86 /appconfig:runcalc.exe.config  calc.cs  /r:"..\..\bin\UITesting.Automated.ControlInf.dll" /r:"..\..\bin\UITesting.Automated.JsonParser.dll" /r:"..\..\bin\UITesting.Automated.JSonSerializer.dll" /r:"..\..\bin\UITesting.Automated.MouseKeyboardActivityMonitor.dll" /r:"..\..\bin\UITesting.Automated.WindowsInput.dll" /r:"..\..\bin\UITesting.Automated.UIADriver.dll" /r:C:\Windows\Microsoft.NET\Framework\v4.0.30319\Microsoft.JScript.dll 
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe  /target:exe /out:runcalc.exe /platform:x86 calc.cs  /r:"..\..\bin\UITesting.Automated.ControlInf.dll" /r:"..\..\bin\UITesting.Automated.JsonParser.dll" /r:"..\..\bin\UITesting.Automated.JSonSerializer.dll" /r:"..\..\bin\UITesting.Automated.MouseKeyboardActivityMonitor.dll" /r:"..\..\bin\UITesting.Automated.WindowsInput.dll" /r:"..\..\bin\UITesting.Automated.UIADriver.dll" /r:C:\Windows\Microsoft.NET\Framework\v4.0.30319\Microsoft.JScript.dll 
 
 pause
